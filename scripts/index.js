89c8b1ebd813da95b86f025e64e56fb91036fca2
YAGO ANDRADE MOLINA <yago.molina@portalsesisp.org.br>
2026-02-11 12:00:59 -0300

    segundo commit

--- scripts/index.js
+++ scripts/index.js
@@ -14,6 +14,52 @@ function validar() {
         mensagem.style.color = "red";
     }
 }
-    
 
+function validarsenha() {
+    const senha1 = document.getElementById("senha1").value;
+    const senha2 = document.getElementById("senha2").value;
+    const mensagemsenha = document.getElementById("mensagemsenha");
+
+    if (senha1.length >= 8){
+        if (senha1 == senha2){
+            mensagemsenha.innerText = "Senha Valida"
+            mensagemsenha.style.color = "green"
+        }
+        else{
+            mensagemsenha.innerText = "Senhas não se coincidem"
+            mensagemsenha.style.color = "red"
+        }
+    }else{
+        mensagemsenha.innerText = "Senha Invalida. Precisa-se de 8 CARACTERES"
+        mensagemsenha.style.color = "red"
+    }
+}
+
+function cargo() {
+    const idade2 = document.getElementById("idade2").value;
+    const estudante = document.getElementById("estudante");
+    const mensagemdesconto = document.getElementById("mensagemdesconto");
+    
+    if (idade2 >= 60 || estudante.checked){
+        mensagemdesconto.innerText = "Você ganhou um desconto!"
+        mensagemdesconto.style.color = "green"
+    } else {
+        mensagemdesconto.innerText = "Você não ganhou desconto"
+        mensagemdesconto.style.color = "red"
+    }
+}
+
+function confirmarnomesenha() {
+    const nome = document.getElementById("Nome1").value;
+    const idade = document.getElementById("idade3").value;
+    const mensagemconfirm = document.getElementById("mensagemconfirm");
+
+    if (nome == "" || idade < 0 || idade == "") {
+        mensagemconfirm.innerText = "Nome invalido ou idade Invalida";
+        mensagemconfirm.style.color = "red";
+    }else {
+        mensagemconfirm.innerText = "Nome/idade confirmados"
+        mensagemconfirm.style.color = "green"
+    }
+}
 
